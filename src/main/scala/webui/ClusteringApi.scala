5cc1710daaf5ec7fa450c8456e22069f7f659fab
Michael Kamm <m.kamm@ssp-europe.eu>
2017-05-16 11:37:33 +0200

    Jar file uploader now reacts on the TrafficLoad Clustering Annotation field

--- src/main/scala/webui/ClusteringApi.scala
+++ src/main/scala/webui/ClusteringApi.scala
@@ -14,7 +14,7 @@ import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.{ByteString, Timeout}
-import clustering.ClusterType
+import clustering.{ClusterType, TrafficLoad}
 import de.oth.clustering.java._
 import spray.json.DefaultJsonProtocol._
 import utils.PrivateMethodExposer
@@ -439,8 +439,13 @@ class ClusteringApi(ip : String) extends Actor with ActorLogging with Directives
             if (a.annotation.clusterType() == ClusterType.GROUPING)
               singleInstance = false
 
+            // check if the executor should be run locally
+            var run_locally = false
+            if(a.annotation.expectedTraffic() == TrafficLoad.MAJOR)
+              run_locally = true
+
             // Add Task to dependency tree
-            instanceActor ! AddTask(datestring, a.annotation.members().toList, Task(loader.getRawTestClass(a.classname), a.classname, a.methodname, singleInstance), version)
+            instanceActor ! AddTask(datestring, a.annotation.members().toList, Task(loader.getRawTestClass(a.classname), a.classname, a.methodname, singleInstance, run_locally), version)
 
             // Add Task to Database
             dBActor ! CreateTask(s"${a.classname}.${a.methodname}", datestring)
