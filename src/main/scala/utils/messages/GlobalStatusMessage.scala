65aba9dbe13862a53b3ae1777f7670bde0a453e9
Oliver Ziegert <oliver@pc-ziegert.de>
2017-05-16 14:59:07 +0200

    Number of VM per Node on GlobasStatusActor

--- src/main/scala/utils/messages/GlobalStatusMessage.scala
+++ src/main/scala/utils/messages/GlobalStatusMessage.scala
@@ -8,6 +8,10 @@ case class RegisterNodeMonitorActor(actor: ActorRef) extends GlobalStatusMessage
 
 case class DeregisterNodeMonitorActor(actor: ActorRef) extends GlobalStatusMessage
 
+case class RegisterVmActor(address: Address) extends GlobalStatusMessage
+
+case class DeregisterVmActor(address: Address) extends GlobalStatusMessage
+
 case class SystemAttributes(attributes: Map[String, String]) extends GlobalStatusMessage
 
 case class GlobalSystemAttributes(globalAttributes : Map[ActorRef, Map[String, String]]) extends GlobalStatusMessage
