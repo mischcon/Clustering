a68921f06bd4032b4b51836b465529d6bd8d6efa
Michael Kamm <m.kamm@ssp-europe.eu>
2017-05-16 11:54:11 +0200

    TestJar script moved to /scripts

--- scripts/create_test_jar.sh
+++ scripts/create_test_jar.sh
@@ -5,16 +5,19 @@ CLUSTERING=$(find ../ -name 'Clustering.jar')
 # check if javac and jar are installed
 type javac >/dev/null 2>&1 || { echo >&2 "I require javac but it's not installed.  Aborting."; exit 1; }
 type jar >/dev/null 2>&1 || { echo >&2 "I require jar but it's not installed.  Aborting."; exit 1; }
-if [ ! -f $CLUSTERING ]; then
+if [ "$CLUSTERING" == "" ] || [ ! -f "$CLUSTERING" ]; then
     echo "Clustering.jar not found, please create it first!"
     exit 1
 fi
-
-rm -rf *.jar *.class
-echo "compiling..."
-javac -cp ".:$CLUSTERING" *.java
-echo "packing..."
-jar cvf TestJar.jar *.class
-rm -rf *.class
-
+rm -rf *.jar
+(
+    echo "compiling..."
+    cd ./.TestJarData
+    rm -rf *.class
+    javac -cp ".:../$CLUSTERING" *.java 1> /dev/null
+    echo "packing..."
+    jar cvf TestJar.jar *.class 1> /dev/null
+    rm -rf *.class
+    mv *.jar ../
+)
 echo "You know can uplaod the generated TestJar.jar file to the cluster!"
